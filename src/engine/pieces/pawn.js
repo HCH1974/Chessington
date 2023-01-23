f88107ec0118a48c7bfdabebbfe0bd9fe1d7f283
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-01-23 14:05:40 +0000

    Add pawn path block restriction

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,16 +11,28 @@ export default class Pawn extends Piece {
         let location = board.findPiece(this)
         const moves = []
         if (this.player === Player.WHITE) {
-            if (location.row === 1) {
+            if (location.row === 1 && board.getPiece(Square.at(location.row + 2, location.col)) === undefined && board.getPiece(Square.at(location.row + 1, location.col)) === undefined) {
                 moves.push(Square.at(location.row + 2, location.col))
             }
-            moves.push(Square.at(location.row + 1, location.col))
+            if (board.getPiece(Square.at(location.row + 1, location.col)) === undefined) {
+                moves.push(Square.at(location.row + 1, location.col))
+            }
+
         } else {
-            if (location.row === 6) {
+            if (location.row === 6 && board.getPiece(Square.at(location.row - 2, location.col)) === undefined && board.getPiece(Square.at(location.row - 1, location.col)) === undefined) {
                 moves.push(Square.at(location.row - 2, location.col))
             }
-            moves.push(Square.at(location.row - 1, location.col))
+            if (board.getPiece(Square.at(location.row - 1, location.col)) === undefined) {
+                moves.push(Square.at(location.row - 1, location.col))
+            }
+
         }
         return moves
     }
 }
+
+
+/*
+if (board.getPiece(Square.at(location.row - 1, location.col)) == undefined) {
+    moves.push(Square.at(location.row - 1, location.col))
+    */
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,3 @@
-import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
 
@@ -10,37 +9,14 @@ export default class Rook extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = []
-        //    (1, 2)
-        /*
-        moves.push(Square.at(location.row - 1, location.col));
-        moves.push(Square.at(location.row + 1, location.col));
-        moves.push(Square.at(location.row + 2, location.col));
-        moves.push(Square.at(location.row + 3, location.col));
-        moves.push(Square.at(location.row + 4, location.col));
-        moves.push(Square.at(location.row + 5, location.col));
-        moves.push(Square.at(location.row + 6, location.col));
+        for (let i = 0; i <= 7; i++) {
+            if (i !== location.row) moves.push(Square.at(i, location.col));
+        }
 
-        moves.push(Square.at(location.row, location.col - 2));
-        moves.push(Square.at(location.row, location.col - 1));
-        moves.push(Square.at(location.row, location.col + 1));
-        moves.push(Square.at(location.row, location.col + 2));
-        moves.push(Square.at(location.row, location.col + 3));
-        moves.push(Square.at(location.row, location.col + 4));
-        moves.push(Square.at(location.row, location.col + 5));
-        */
+        for (let i = 0; i <= 7; i++) {
+            if (i !== location.col) moves.push(Square.at(location.row, i));
+        }
 
-        for (let i = 0; i < location.row; i++) {
-            moves.push(Square.at(i, location.col));
-        }
-        for (let i = location.row + 1; i < 8; i++) {
-            moves.push(Square.at(i, location.col));
-        }
-        for (let i = 0; i < location.col; i++) {
-            moves.push(Square.at(location.row, i));
-        }
-        for (let i = location.col + 1; i < 8; i++) {
-            moves.push(Square.at(location.row, i));
-        }
 
         return moves;
     }
