042a63a289bd0f0c42145229f378e9831ad91f45
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-01-23 15:54:40 +0000

    Add work so far

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,3 +1,4 @@
+import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
 
@@ -25,6 +26,11 @@ export default class Rook extends Piece {
 
         for (let i = location.col + 1; i <= 7; i++) {
             if (board.getPiece(Square.at(location.row, i)) === undefined) {
+                moves.push(Square.at(location.row, i));
+            }
+
+            // TODO - change to not moving through, just taking
+            else if (board.getPiece(Square.at(location.row, i)).player === Player.BLACK) {
                 moves.push(Square.at(location.row, i));
             }
             else break;
